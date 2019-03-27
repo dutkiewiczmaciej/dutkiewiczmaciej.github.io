5abee08c0a5eb86348ebfb9e72945a9352c320fa
dutkiewiczmaciej <46227827+dutkiewiczmaciej@users.noreply.github.com>
2019-03-28 00:41:56 +0100

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -133,13 +133,34 @@ $(document).ready(function() {
       });
 
       $('.gallery4').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         dots: false,
         pauseOnDotsHover: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
       });
-
+            
 });
 
